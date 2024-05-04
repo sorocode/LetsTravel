b403eea7f7bf308e237328b5eef9b594d20de38b
Soroso <tudgus010707@naver.com>
2024-05-04 23:59:47 +0900

    🌱Initial commit

---
+++ src/main/java/com/letsTravel/LetsTravel/LetsTravelApplication.java
@@ -0,0 +1,13 @@
+package com.letsTravel.LetsTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letsTravel/LetsTravel/LetsTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letsTravel.LetsTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
