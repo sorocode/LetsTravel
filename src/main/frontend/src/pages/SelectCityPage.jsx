90afdbbb44d171b4b1e39fd79404b05a284de15c
Soroso <tudgus010707@naver.com>
2024-05-06 00:39:07 +0900

    ✨ 테스트 코드 추가

--- src/main/frontend/src/pages/SelectCityPage.jsx
+++ src/main/frontend/src/pages/SelectCityPage.jsx
@@ -1,9 +1,24 @@
-import { useSearchParams } from "react-router-dom";
-
+import { useDispatch, useSelector } from "react-redux";
+import { dummyCities } from "../dummyCities";
+import { addCity } from "../store/schedule/scheduleSlice";
 const SelectCityPage = () => {
+  const cities = useSelector((state) => state.schedule.cities);
+  const dispatch = useDispatch();
   return (
     <div>
-      <h1>국가 선택</h1>
+      <h1>도시 선택</h1>
+      {dummyCities.map((city) => (
+        <p key={city.id}>
+          <button onClick={() => dispatch(addCity(city))}>
+            {city.cityName}
+          </button>
+        </p>
+      ))}
+      <br />
+      <h2>현재 {cities.length}개 국가 선택중</h2>
+      {cities.map((city, index) => (
+        <p key={index}>{city.cityName}</p>
+      ))}
     </div>
   );
 };
