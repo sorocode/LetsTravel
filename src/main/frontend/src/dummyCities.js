e42cea69705077c9fc5b1a593d57a412ae6ac587
Soroso <tudgus010707@naver.com>
2024-05-06 00:38:14 +0900

    🍱 더미 도시 데이터 추가

---
+++ src/main/frontend/src/dummyCities.js
@@ -0,0 +1,8 @@
+export const dummyCities = [
+  { id: 9040331, cityName: "hanoi", countryName: "vn" },
+  { id: 1023191, cityName: "new york", countryName: "us" },
+  { id: 1012738, cityName: "bangkok", countryName: "th" },
+  { id: 1006886, cityName: "london", countryName: "gb" },
+  { id: 1009317, cityName: "tokyo", countryName: "jp" },
+  { id: 1003334, cityName: "beijing", countryName: "cn" },
+];
