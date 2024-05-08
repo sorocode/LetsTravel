53067233d7e1eed07555a0e7e85ae7d0ef2da143
Soroso <tudgus010707@naver.com>
2024-05-08 14:39:15 +0900

    ✨기본 날짜 추가

--- src/main/frontend/src/components/UI/DateInput.jsx
+++ src/main/frontend/src/components/UI/DateInput.jsx
@@ -1,10 +1,11 @@
-const DateInput = ({ isStart }) => {
+const DateInput = ({ isStart, dateValue }) => {
   return (
     <input
       className="w-64 h-11 border-gray-800 border rounded-2xl px-4"
       type="date"
       name={isStart ? "startDate" : "endDate"}
       id={isStart ? "startDate" : "endDate"}
+      value={dateValue}
     />
   );
 };
--- src/main/frontend/src/components/UI/SelectDateArea.jsx
+++ src/main/frontend/src/components/UI/SelectDateArea.jsx
@@ -1,16 +1,18 @@
 import DateInput from "./DateInput";
 import Button from "./Buttons/Button";
-
+import { useSelector } from "react-redux";
 const SelectDateArea = () => {
+  const startDate = useSelector((state) => state.schedule.startDate);
+  const endDate = useSelector((state) => state.schedule.endDate);
   return (
     <div className="flex flex-col gap-4 items-center">
       <div className="flex items-center gap-2">
         <span>🛫 출발</span>
-        <DateInput isStart />
+        <DateInput isStart dateValue={startDate} />
       </div>
       <div className="flex items-center gap-2">
         <span>🛬 도착</span>
-        <DateInput isStart={false} />
+        <DateInput isStart={false} dateValue={endDate} />
       </div>
       <div>
         <Button color="FCD4FF">다음 단계</Button>
