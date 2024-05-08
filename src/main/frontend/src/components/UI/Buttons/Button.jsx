9db8bdbcb2f4d64c92f286bc4334de6a6d87f9ab
Soroso <tudgus010707@naver.com>
2024-05-08 14:09:25 +0900

    chore: 안쓰는 코드 삭제

--- src/main/frontend/src/components/UI/Buttons/Button.jsx
+++ src/main/frontend/src/components/UI/Buttons/Button.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { Link } from "react-router-dom";
 function Button({ color, children, ...props }) {
   let cssClass = `flex px-4 py-2 rounded-2xl justify-center items-center w-32 mt-6 font-semibold shadow-md hover:opacity-80 active:opacity-80`;
