fab11a0209f7ed70651980fcd008c7b421d4f86f
Soroso <tudgus010707@naver.com>
2024-05-08 14:06:45 +0900

    🐛 backgroundColor 안나오는 문제 재해결

--- src/main/frontend/src/components/UI/Buttons/Button.jsx
+++ src/main/frontend/src/components/UI/Buttons/Button.jsx
@@ -1,12 +1,19 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 function Button({ color, children, ...props }) {
-  let cssClass = `flex px-4 py-2 rounded-2xl justify-center items-center w-32 mt-6 font-semibold shadow-md active:opacity-80`;
-  cssClass += ` bg-[#${color}]`;
+  let cssClass = `flex px-4 py-2 rounded-2xl justify-center items-center w-32 mt-6 font-semibold shadow-md hover:opacity-80 active:opacity-80`;
+
   return (
-    <Link className={cssClass} {...props}>
+    <Link
+      style={{ backgroundColor: `#${color}` }}
+      className={cssClass}
+      {...props}
+    >
       {children}
     </Link>
   );
 }
-
+Button.defaultProps = {
+  color: "fcd4ff",
+};
 export default Button;
