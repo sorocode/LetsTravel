c1d368804d6b499864501721ee55aeef91e9b3ca
Soroso <tudgus010707@naver.com>
2024-05-08 10:44:50 +0900

    🐛 background color 안나오는 오류 수정

--- src/main/frontend/src/components/UI/Buttons/Button.jsx
+++ src/main/frontend/src/components/UI/Buttons/Button.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 function Button({ color, children, ...props }) {
-  let cssClass = `flex px-4 py-2 rounded-2xl justify-center items-center bg-[#${color}] w-32 mt-6 font-semibold`;
-
+  let cssClass = `flex px-4 py-2 rounded-2xl justify-center items-center w-32 mt-6 font-semibold shadow-md active:opacity-80`;
+  cssClass += ` bg-[#${color}]`;
   return (
     <Link className={cssClass} {...props}>
       {children}
