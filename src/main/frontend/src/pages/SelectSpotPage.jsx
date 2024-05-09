1b503a5e6723d05fa6bfbd4edf4c752172c00677
Soroso <tudgus010707@naver.com>
2024-05-10 00:13:42 +0900

    ✨바텀시트 추가

--- src/main/frontend/src/pages/SelectSpotPage.jsx
+++ src/main/frontend/src/pages/SelectSpotPage.jsx
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom";
 import SearchResults from "../components/UI/SearchResults";
 import { dummyCities } from "../dummyCities";
-import CityItem from "../components/UI/CityItem";
-
+import Button from "../components/UI/Buttons/Button";
+import SpotItem from "../components/UI/SpotItem";
+import { AnimatePresence, motion } from "framer-motion";
+import BottomSheet from "../components/UI/Bottomsheet/BottomSheet";
 function SelectSpotPage() {
   const params = useParams();
   const selectedCity = dummyCities.find(
@@ -15,19 +17,48 @@ function SelectSpotPage() {
         {(item) => {
           const isSelected = JSON.stringify(selectedCity).includes(item.id);
           return (
-            <CityItem
+            <SpotItem
               key={item.id}
-              cityName={item.spotName}
-              cityCountry={item.spotName}
+              spotName={item.spotName}
+              spotCountry={item.spotName}
+              latitude={item.latitude}
+              longitude={item.longitude}
               isSelectMode={true}
               // onClick={() => {
               //   isSelected ? handleRemove(item) : handleAdd(item);
               // }}
-              isSelected={isSelected}
+              isSelected={false}
             />
           );
         }}
       </SearchResults>
+      <BottomSheet title={<h2>바텀시트</h2>}>
+        <div className="flex flex-col gap-2 justify-center items-center mt-2">
+          <AnimatePresence>
+            TODO: 아래 주석 참고해서 선택 아이템 표시
+            {/* {cities.map((city, index) => (
+              <motion.span
+                key={index}
+                className="font-bold w-1/2"
+                variants={{
+                  hidden: { opacity: 0, scale: 0.5 },
+                  visible: { opacity: 1, scale: 1 },
+                }}
+                initial="hidden"
+                animate="visible"
+                exit={{ opacity: 0, scale: 0.5 }}
+              >
+                {city.cityName.toUpperCase()}
+              </motion.span>
+            ))} */}
+          </AnimatePresence>
+        </div>
+        <div className="flex justify-center">
+          <Button color="#FCD4FF" to=".">
+            다음 단계
+          </Button>
+        </div>
+      </BottomSheet>
     </div>
   );
 }
