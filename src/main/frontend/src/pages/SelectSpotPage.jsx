434294ea44ba866a4d19f67b3963ac301799edc0
Soroso <tudgus010707@naver.com>
2024-05-09 10:03:32 +0900

    ✨도시 선택 여부에 따라 폴백텍스트 변경

--- src/main/frontend/src/pages/SelectSpotPage.jsx
+++ src/main/frontend/src/pages/SelectSpotPage.jsx
@@ -2,11 +2,14 @@ import { useParams } from "react-router-dom";
 
 function SelectSpotPage() {
   const params = useParams();
+
   return (
     <div>
       <h1>여행스팟을 선택하는 페이지입니다.</h1>
       <p>
-        현재 선택된 장소: <b>{params.city}</b>
+        {params.city.includes("noCity") &&
+          "도시가 선택되지 않았습니다. 도시를 선택해주세요."}
+        {!params.city.includes("noCity") && `현재 선택된 장소: ${params.city}`}
       </p>
     </div>
   );
