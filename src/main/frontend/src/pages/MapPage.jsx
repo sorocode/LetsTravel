f4a3899e0c80cdd755cb817a9d0ccf4b0aa9e212
Soroso <tudgus010707@naver.com>
2024-05-10 18:28:33 +0900

    ✨ 지도 컴포넌트 추가

--- src/main/frontend/src/pages/MapPage.jsx
+++ src/main/frontend/src/pages/MapPage.jsx
@@ -1,5 +1,12 @@
+import MapContainer from "../components/UI/MapContainer";
+
 function MapPage() {
-  return <h1>지도가 나타나는 페이지입니다.</h1>;
+  return (
+    <>
+      <h1>지도 화면</h1>
+      <MapContainer />
+    </>
+  );
 }
 
 export default MapPage;
