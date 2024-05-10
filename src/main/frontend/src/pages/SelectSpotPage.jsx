9a0fac28508e60520c1c7123a1f99d4ecb66d652
Soroso <tudgus010707@naver.com>
2024-05-10 14:19:25 +0900

    ✨여행지 선택

--- src/main/frontend/src/pages/SelectSpotPage.jsx
+++ src/main/frontend/src/pages/SelectSpotPage.jsx
@@ -5,28 +5,36 @@ import Button from "../components/UI/Buttons/Button";
 import SpotItem from "../components/UI/SpotItem";
 import { AnimatePresence, motion } from "framer-motion";
 import BottomSheet from "../components/UI/Bottomsheet/BottomSheet";
+import { useDispatch, useSelector } from "react-redux";
+import { addSpot, removeSpot } from "../store/schedule/scheduleSlice";
 function SelectSpotPage() {
   const params = useParams();
+  const spots = useSelector((state) => state.schedule.spots);
+  const dispatch = useDispatch();
   const selectedCity = dummyCities.find(
     (city) => city.cityName === params.city
   );
-
+  const handleAddSpot = (item) => {
+    dispatch(addSpot(item));
+  };
+  const handleRemoveSpot = (item) => {
+    dispatch(removeSpot(item.id));
+  };
   return (
     <div>
       <SearchResults items={selectedCity.spots} searchId="spots">
         {(item) => {
-          const isSelected = JSON.stringify(selectedCity).includes(item.id);
+          const isSelected = JSON.stringify(spots).includes(item.id);
+          console.log("dj", isSelected);
           return (
             <SpotItem
               key={item.id}
               spotName={item.spotName}
-              spotCountry={item.spotName}
               latitude={item.latitude}
               longitude={item.longitude}
-              isSelectMode={true}
-              // onClick={() => {
-              //   isSelected ? handleRemove(item) : handleAdd(item);
-              // }}
+              onClick={() => {
+                isSelected ? handleRemoveSpot(item) : handleAddSpot(item);
+              }}
               isSelected={false}
             />
           );
@@ -36,7 +44,7 @@ function SelectSpotPage() {
         <div className="flex flex-col gap-2 justify-center items-center mt-2">
           <AnimatePresence>
             TODO: 아래 주석 참고해서 선택 아이템 표시
-            {/* {cities.map((city, index) => (
+            {spots.map((spot, index) => (
               <motion.span
                 key={index}
                 className="font-bold w-1/2"
@@ -48,9 +56,9 @@ function SelectSpotPage() {
                 animate="visible"
                 exit={{ opacity: 0, scale: 0.5 }}
               >
-                {city.cityName.toUpperCase()}
+                {spot.spotName.toUpperCase()}
               </motion.span>
-            ))} */}
+            ))}
           </AnimatePresence>
         </div>
         <div className="flex justify-center">
