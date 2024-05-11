e7d991eac04b91fa0534d27496c1cbb9ab2226e7
Soroso <tudgus010707@naver.com>
2024-05-11 22:46:50 +0900

    🎨 queryClient 위치 http.js로 변경

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./util/router";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./util/http";
 function App() {
-  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
