927cf5b4f3994291ccf4bb7728849059be1b7618
Soroso <tudgus010707@naver.com>
2024-05-13 11:28:31 +0900

    💄인트로 페이지 스타일링

--- src/main/frontend/src/pages/IntroPage.jsx
+++ src/main/frontend/src/pages/IntroPage.jsx
@@ -2,7 +2,8 @@ import Button from "../components/UI/Buttons/Button";
 
 const IntroPage = () => {
   return (
-    <div className="flex justify-center items-center mt-30">
+    <div className="flex flex-col justify-center items-center mt-36">
+      <h1 className="text-2xl font-bold">LetsTravel</h1>
       <Button to="select" color="#B7E2FF">
         ✈️여행 일정 만들러가기
       </Button>
