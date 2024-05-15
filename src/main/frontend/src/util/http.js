0b71ef7ea9021cbdec856ecefb44790906457fa6
Soroso <tudgus010707@naver.com>
2024-05-15 18:15:30 +0900

    🎨 http 조율

--- src/main/frontend/src/util/http.js
+++ src/main/frontend/src/util/http.js
@@ -3,7 +3,7 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient();
 
 const URL = "http://localhost:8080/api";
-const PLACE_TEXT_URL = "https://places.googleapis.com/v1/places:";
+const PLACE_TEXT_URL = "https://places.googleapis.com/v1/places:searchText";
 const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_KEY;
 // countryCode 보내면 도시 불러옴
 export const fetchCities = async (countryCode) => {
@@ -18,14 +18,20 @@ export const fetchCities = async (countryCode) => {
 };
 
 //Place API 텍스트 검색 관련 로직
-export const fetchSpots = async (searchTerm) => {
+export const fetchSpots = async (searchTerm, city) => {
   try {
-    const req = await axios.post(PLACE_TEXT_URL + `${searchTerm}`, {
-      headers: {
-        "Content-Type": "application/json",
-        "X-Goog-Api-Key": GOOGLE_API_KEY,
-      },
-    });
+    const req = await axios.post(
+      PLACE_TEXT_URL,
+      { textQuery: `${searchTerm} in ${city}`, languageCode: "ko" },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-Goog-Api-Key": GOOGLE_API_KEY,
+          "X-Goog-FieldMask":
+            "places.displayName,places.formattedAddress,places.priceLevel,places.location",
+        },
+      }
+    );
     // console.log(req);
     return req.data;
   } catch (err) {
