738ccd0dd7dce0270e1134c797cd9f6b1bbbf997
Soroso <tudgus010707@naver.com>
2024-05-15 17:19:44 +0900

    ✨ 구글맵 api 로직 추가

--- src/main/frontend/src/util/http.js
+++ src/main/frontend/src/util/http.js
@@ -3,6 +3,8 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient();
 
 const URL = "http://localhost:8080/api";
+const PLACE_TEXT_URL = "https://places.googleapis.com/v1/places:";
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_KEY;
 // countryCode 보내면 도시 불러옴
 export const fetchCities = async (countryCode) => {
   try {
@@ -11,6 +13,23 @@ export const fetchCities = async (countryCode) => {
     return req.data;
   } catch (err) {
     const fetchError = new Error("도시 불러오기 실패");
+    throw fetchError;
+  }
+};
+
+//Place API 텍스트 검색 관련 로직
+export const fetchSpots = async (searchTerm) => {
+  try {
+    const req = await axios.post(PLACE_TEXT_URL + `${searchTerm}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": GOOGLE_API_KEY,
+      },
+    });
+    // console.log(req);
+    return req.data;
+  } catch (err) {
+    const fetchError = new Error("여행지 불러오기 실패");
     throw fetchError;
   }
 };
