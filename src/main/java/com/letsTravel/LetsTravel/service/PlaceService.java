f1fd83875ca1f1de061eae5cdf7ea7f5711e265c
KoharuDaisukii <bong1926@naver.com>
2024-05-19 01:18:59 +0900

    createPlaces에서 Place를 저장하도록 추가 및 build 과정 삭제

--- src/main/java/com/letsTravel/LetsTravel/service/PlaceService.java
+++ src/main/java/com/letsTravel/LetsTravel/service/PlaceService.java
@@ -1,10 +1,12 @@
 package com.letsTravel.LetsTravel.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.letsTravel.LetsTravel.domain.CityCreateDTO;
-import com.letsTravel.LetsTravel.domain.PlaceCreateDTO;
+import com.letsTravel.LetsTravel.domain.GoogleMapsPlace;
 import com.letsTravel.LetsTravel.repository.PlaceRepository;
 
 @Service
@@ -19,16 +21,23 @@ public class PlaceService {
 		this.cityService = cityService;
 	}
 
-	public int createPlace(PlaceCreateDTO placeCreateDTO) {
-		// City가 저장되어 있는지 확인
-		CityCreateDTO cityCreateDTO = CityCreateDTO.builder()
-				.countryCode(placeCreateDTO.getCountryCode())
-				.cityName(placeCreateDTO.getCityName())
-				.cityLanguageCode(placeCreateDTO.getCityLanguageCode())
-				.build();
-		cityService.addCity(cityCreateDTO);
+	public int createPlaces(List<GoogleMapsPlace> GoogleMapsPlaceList) {
 		
-		return 0;
+		int result = 0;
+		
+		for (int i = 0; i < GoogleMapsPlaceList.size(); i++) {
+			// City 저장(없으면 저장, 있으면 패스)
+			List<CityCreateDTO> cityCreateDTOList = GoogleMapsPlaceList.get(i).getCities();
+			for (int j = 0; j < cityCreateDTOList.size(); j++)
+				cityService.addCity(cityCreateDTOList.get(i));
+			
+			// Place 저장
+			result += placeRepository.addPlace(GoogleMapsPlaceList.get(i).getPlaceDetail());
+			
+			// Type 번역
+		}
+
+		return result;
 	}
 
 }
