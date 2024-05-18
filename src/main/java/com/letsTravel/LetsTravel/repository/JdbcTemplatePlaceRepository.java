5b124ae296bc83e59651a9f786461bb369bd28ce
KoharuDaisukii <bong1926@naver.com>
2024-05-19 01:15:18 +0900

    Place record 추가하는 기능 구현

--- src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplatePlaceRepository.java
+++ src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplatePlaceRepository.java
@@ -5,20 +5,26 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.letsTravel.LetsTravel.domain.GoogleMapsPlace;
 import com.letsTravel.LetsTravel.domain.PlaceCreateDTO;
 
 @Repository
-public class JdbcTemplatePlaceRepository implements PlaceRepository{
+public class JdbcTemplatePlaceRepository implements PlaceRepository {
 
 	private final JdbcTemplate jdbcTemplate;
-	
+
 	public JdbcTemplatePlaceRepository(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
-	
+
 	@Override
 	public int addPlace(PlaceCreateDTO placeCreateDTO) {
-		return 1;
+		String sql = "INSERT INTO Place(Place_id, Place_name, Place_formatted_address, Place_latitude, Place_longitude, Place_gmap_uri, Place_insert_date) "
+				+ "VALUES(?, ?, ?, ?, ?, ?, curdate()) " + "ON DUPLICATE KEY UPDATE Place_insert_date = curdate();";
+		return jdbcTemplate.update(sql, placeCreateDTO.getId(), placeCreateDTO.getDisplayName(),
+				placeCreateDTO.getFormattedAddress(), placeCreateDTO.getLocation().getLatitude(),
+				placeCreateDTO.getLocation().getLongitude(), placeCreateDTO.getGoogleMapsUri());
+
 	}
 
 }
