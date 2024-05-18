98b493b2ea11b78f0ef5f6d30927c0c92cb35843
KoharuDaisukii <bong1926@naver.com>
2024-05-19 01:13:21 +0900

    latitude와 longitude를 field로 갖는 class

---
+++ src/main/java/com/letsTravel/LetsTravel/domain/Location.java
@@ -0,0 +1,14 @@
+package com.letsTravel.LetsTravel.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Location {
+
+	private float latitude;
+	private float longitude;
+}
