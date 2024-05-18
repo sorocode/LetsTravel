d1d701b51acb769e8575a5fc1f7ef41ffed14b8b
KoharuDaisukii <bong1926@naver.com>
2024-05-18 16:24:08 +0900

    City record 추가하는 method 추가

--- src/main/java/com/letsTravel/LetsTravel/repository/CityRepository.java
+++ src/main/java/com/letsTravel/LetsTravel/repository/CityRepository.java
@@ -2,9 +2,11 @@ package com.letsTravel.LetsTravel.repository;
 
 import java.util.List;
 
+import com.letsTravel.LetsTravel.domain.CityCreateDTO;
 import com.letsTravel.LetsTravel.domain.CityDTO;
 
 public interface CityRepository {
 
 	public List<CityDTO> findCities(String countryCode);
+	public int addCity(CityCreateDTO cityCreateDTO);
 }
