66282c707a8fc4eb603fb6fe88d0cb57077e3f10
Soroso <tudgus010707@naver.com>
2024-05-19 20:12:32 +0900

    🐛 검색결과 클릭하면 모드 바뀌는 오류 수정

--- src/main/frontend/src/components/UI/SearchBar.jsx
+++ src/main/frontend/src/components/UI/SearchBar.jsx
@@ -2,12 +2,13 @@ import { forwardRef } from "react";
 import searchIcon from "../../assets/icons/search_icon.svg";
 
 const SearchBar = forwardRef(function SearchBar(
-  { onSubmit, searchBarId, placeHolder, ...props },
+  { onChangeMode, onSubmit, searchBarId, placeHolder, ...props },
   ref
 ) {
   return (
     <form
       onSubmit={onSubmit}
+      onClick={onChangeMode}
       className="container flex border-gray-600 border-[0.9px] w-4/5 rounded-xl justify-between px-4 py-2 mt-20 mb-5"
     >
       <input
