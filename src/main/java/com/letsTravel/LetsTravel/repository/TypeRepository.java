fa95529094f0be11ad4b56c543fb36450ea9a27e
KoharuDaisukii <bong1926@naver.com>
2024-05-19 21:46:50 +0900

    Create TypeRepository.java
    Type 번역용 repository

---
+++ src/main/java/com/letsTravel/LetsTravel/repository/TypeRepository.java
@@ -0,0 +1,8 @@
+package com.letsTravel.LetsTravel.repository;
+
+import com.letsTravel.LetsTravel.domain.TypeTranslateDTO;
+
+public interface TypeRepository {
+
+	public int modifyTypeNameTranslated(TypeTranslateDTO typeTranslateDTO);
+}
