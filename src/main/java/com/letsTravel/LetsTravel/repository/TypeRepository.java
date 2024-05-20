bdc30b2aeb99b16dfc332ec42af616be5b002e89
KoharuDaisukii <bong1926@naver.com>
2024-05-20 19:52:55 +0900

    Place type 레코드를 추가하기 위한 abstract method 추가

--- src/main/java/com/letsTravel/LetsTravel/repository/TypeRepository.java
+++ src/main/java/com/letsTravel/LetsTravel/repository/TypeRepository.java
@@ -1,8 +1,10 @@
 package com.letsTravel.LetsTravel.repository;
 
+import com.letsTravel.LetsTravel.domain.PlaceTypeCreateDTO;
 import com.letsTravel.LetsTravel.domain.TypeTranslateDTO;
 
 public interface TypeRepository {
 
 	public int modifyTypeNameTranslated(TypeTranslateDTO typeTranslateDTO);
+	public int addPlaceType(PlaceTypeCreateDTO type);
 }
