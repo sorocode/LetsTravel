b296dddad75ebf414fec1a4f86e2854997753cbf
KoharuDaisukii <bong1926@naver.com>
2024-05-20 19:47:48 +0900

    Place_seq를 받기 위해 stored procedure를 실행하도록 구현

--- src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplatePlaceRepository.java
+++ src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplatePlaceRepository.java
@@ -1,30 +1,39 @@
 package com.letsTravel.LetsTravel.repository;
 
+import java.util.Map;
+
 import javax.sql.DataSource;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 
-import com.letsTravel.LetsTravel.domain.GoogleMapsPlace;
 import com.letsTravel.LetsTravel.domain.PlaceCreateDTO;
 
 @Repository
 public class JdbcTemplatePlaceRepository implements PlaceRepository {
 
-	private final JdbcTemplate jdbcTemplate;
+	private final SimpleJdbcCall simpleJdbcCall;
 
+	// Stored Procedure를 쓰는 게 맞나?
 	public JdbcTemplatePlaceRepository(DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		this.simpleJdbcCall = new SimpleJdbcCall(dataSource).withProcedureName("add_place_proc");
 	}
 
 	@Override
 	public int addPlace(PlaceCreateDTO placeCreateDTO) {
-		String sql = "INSERT INTO Place(Place_id, Place_name, Place_formatted_address, Place_latitude, Place_longitude, Place_gmap_uri, Place_insert_date) "
-				+ "VALUES(?, ?, ?, ?, ?, ?, curdate()) " + "ON DUPLICATE KEY UPDATE Place_insert_date = curdate();";
-		return jdbcTemplate.update(sql, placeCreateDTO.getId(), placeCreateDTO.getDisplayName(),
-				placeCreateDTO.getFormattedAddress(), placeCreateDTO.getLocation().getLatitude(),
-				placeCreateDTO.getLocation().getLongitude(), placeCreateDTO.getGoogleMapsUri());
-
+		// 이건 ON UPDATE DUPLICATE KEY가 맞음
+		SqlParameterSource in = new MapSqlParameterSource()
+				.addValue("in_id", placeCreateDTO.getId())
+				.addValue("in_name", placeCreateDTO.getDisplayName())
+				.addValue("in_formatted_address", placeCreateDTO.getFormattedAddress())
+				.addValue("in_latitude", placeCreateDTO.getLocation().getLatitude())
+				.addValue("in_longitude", placeCreateDTO.getLocation().getLongitude())
+				.addValue("in_gmap_uri", placeCreateDTO.getGoogleMapsUri());
+		
+		Map out = simpleJdbcCall.execute(in);
+		return (int)out.get("out_place_seq");
 	}
 
 }
