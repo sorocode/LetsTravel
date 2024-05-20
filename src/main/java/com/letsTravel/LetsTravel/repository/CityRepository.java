5555eb41e13ba90923e5384538130781fc901391
KoharuDaisukii <bong1926@naver.com>
2024-05-20 19:52:05 +0900

    Place_city 레코드를 추가하기 위한 abstract method 추가

--- src/main/java/com/letsTravel/LetsTravel/repository/CityRepository.java
+++ src/main/java/com/letsTravel/LetsTravel/repository/CityRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import com.letsTravel.LetsTravel.domain.CityCreateDTO;
 import com.letsTravel.LetsTravel.domain.CityDTO;
+import com.letsTravel.LetsTravel.domain.PlaceCityCreateDTO;
 
 public interface CityRepository {
 
 	public List<CityDTO> findCities(String countryCode);
 	public int addCity(CityCreateDTO cityCreateDTO);
+	public int addPlaceCity(PlaceCityCreateDTO placeCityCreateDTO);
 }
