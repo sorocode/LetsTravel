62e96d314538bc6134183ebaed304a340b85d42c
KoharuDaisukii <bong1926@naver.com>
2024-05-20 19:51:16 +0900

    Update JdbcTemplateTypeRepository.java
    1. modifyTypeNameTranslated 메소드의 쿼리에 대한 profiling 결과를 주석으로 달아 놓음
    2. Place_type record insert 구현

--- src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplateTypeRepository.java
+++ src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplateTypeRepository.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.letsTravel.LetsTravel.domain.PlaceTypeCreateDTO;
 import com.letsTravel.LetsTravel.domain.TypeTranslateDTO;
 
 @Repository
@@ -18,8 +19,16 @@ public class JdbcTemplateTypeRepository implements TypeRepository {
 
 	@Override
 	public int modifyTypeNameTranslated(TypeTranslateDTO typeTranslateDTO) {
+		// Profiling 해보니 Type_name_translated IS NULL 확인하는 게 update 할 때 더 손해라 안 넣는 게 나을 듯
+		// Short-circuit evaluation을 하기 때문인 듯
 		String sql = "UPDATE Type SET Type_name_translated = ? WHERE Type_name = ?;";
 		return jdbcTemplate.update(sql, typeTranslateDTO.getPrimaryTypeDisplayName(), typeTranslateDTO.getPrimaryType());
+	}
+
+	@Override
+	public int addPlaceType(PlaceTypeCreateDTO placeTypeCreateDTO) {
+		String sql = "INSERT IGNORE INTO Place_type VALUES(?, (SELECT Type_seq FROM Type WHERE Type_name = ?), ?) ;";
+		return jdbcTemplate.update(sql, placeTypeCreateDTO.getPlaceSeq(), placeTypeCreateDTO.getType(), placeTypeCreateDTO.isPrimaryType());
 	}
 
 }
