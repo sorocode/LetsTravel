0d6c6447554e57ecf2e7c43069628ce6b9b4a51d
KoharuDaisukii <bong1926@naver.com>
2024-05-20 20:53:01 +0900

    Update PlaceService.java
    1. CityService에 의존하던 것을 CityRepository로 변경
    2. createPlaces method implementation

--- src/main/java/com/letsTravel/LetsTravel/service/PlaceService.java
+++ src/main/java/com/letsTravel/LetsTravel/service/PlaceService.java
@@ -4,9 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.letsTravel.LetsTravel.domain.CityCreateDTO;
 import com.letsTravel.LetsTravel.domain.GoogleMapsPlace;
+import com.letsTravel.LetsTravel.domain.PlaceCityCreateDTO;
+import com.letsTravel.LetsTravel.domain.PlaceTypeCreateDTO;
+import com.letsTravel.LetsTravel.domain.PrimaryTypeUpdateDTO;
+import com.letsTravel.LetsTravel.repository.CityRepository;
 import com.letsTravel.LetsTravel.repository.PlaceRepository;
 import com.letsTravel.LetsTravel.repository.TypeRepository;
 
@@ -14,34 +19,44 @@ import com.letsTravel.LetsTravel.repository.TypeRepository;
 public class PlaceService {
 
 	private final PlaceRepository placeRepository;
-	private final CityService cityService;
+	private final CityRepository cityRepository;
 	private final TypeRepository typeRepository;
 
 	@Autowired
-	public PlaceService(PlaceRepository placeRepository, CityService cityService, TypeRepository typeRepository) {
+	public PlaceService(PlaceRepository placeRepository, CityRepository cityRepository, TypeRepository typeRepository) {
 		this.placeRepository = placeRepository;
-		this.cityService = cityService;
+		this.cityRepository = cityRepository;
 		this.typeRepository = typeRepository;
 	}
-
-	public int createPlaces(List<GoogleMapsPlace> GoogleMapsPlaceList) {
-		
-		int result = 0;
-		
-		for (int i = 0; i < GoogleMapsPlaceList.size(); i++) {
+	
+	@Transactional
+	public int createPlaces(List<GoogleMapsPlace> googleMapsPlaceList) {
+		for (int placeIndex = 0; placeIndex < googleMapsPlaceList.size(); placeIndex++) {
 			// City 저장(없으면 저장, 있으면 패스)
-			List<CityCreateDTO> cityCreateDTOList = GoogleMapsPlaceList.get(i).getCities();
-			for (int j = 0; j < cityCreateDTOList.size(); j++)
-				cityService.addCity(cityCreateDTOList.get(i));
-			
+			List<CityCreateDTO> cityCreateDTOList = googleMapsPlaceList.get(placeIndex).getCities();
+			for (int cityIndex = 0; cityIndex < cityCreateDTOList.size(); cityIndex++)
+				cityRepository.addCity(cityCreateDTOList.get(cityIndex));
+
 			// Place 저장
-			result += placeRepository.addPlace(GoogleMapsPlaceList.get(i).getPlaceDetail());
-			
+			int placeSeq = placeRepository.addPlace(googleMapsPlaceList.get(placeIndex).getPlaceDetail());
+
+			// Place의 City 저장
+			for (int cityIndex = 0; cityIndex < cityCreateDTOList.size(); cityIndex++)
+				cityRepository.addPlaceCity(new PlaceCityCreateDTO(placeSeq, cityCreateDTOList.get(cityIndex)));
+
+			// Place의 Type 저장
+			List<String> types = googleMapsPlaceList.get(placeIndex).getTypes();
+			for (int typeIndex = 0; typeIndex < types.size(); typeIndex++)
+				typeRepository.addPlaceType(new PlaceTypeCreateDTO(placeSeq, types.get(typeIndex)));
+
+			// Place의 Primary Type 설정
+			typeRepository.modifyPrimaryType(new PrimaryTypeUpdateDTO(placeSeq,
+					googleMapsPlaceList.get(placeIndex).getPrimaryTypeDetail().getPrimaryType()));
+
 			// Type 번역
-			typeRepository.modifyTypeNameTranslated(GoogleMapsPlaceList.get(i).getPrimaryTypeDetail());
+			typeRepository.modifyTypeNameTranslated(googleMapsPlaceList.get(placeIndex).getPrimaryTypeDetail());
 		}
 
-		return result;
+		return 200;
 	}
-
 }
