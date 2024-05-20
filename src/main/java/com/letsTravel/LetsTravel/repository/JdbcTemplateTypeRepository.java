389c3361ff7d2941c54be860106a787629028d8c
KoharuDaisukii <bong1926@naver.com>
2024-05-20 20:51:20 +0900

    Update JdbcTemplateTypeRepository.java
    1. TypeTranslateDTO -> PrimaryTypeDetailDTO
    2. Place의 Type 추가와 Place의 Primary Type 설정을 분리

--- src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplateTypeRepository.java
+++ src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplateTypeRepository.java
@@ -6,7 +6,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.letsTravel.LetsTravel.domain.PlaceTypeCreateDTO;
-import com.letsTravel.LetsTravel.domain.TypeTranslateDTO;
+import com.letsTravel.LetsTravel.domain.PrimaryTypeUpdateDTO;
+import com.letsTravel.LetsTravel.domain.PrimaryTypeDetailDTO;
 
 @Repository
 public class JdbcTemplateTypeRepository implements TypeRepository {
@@ -18,17 +19,23 @@ public class JdbcTemplateTypeRepository implements TypeRepository {
 	}
 
 	@Override
-	public int modifyTypeNameTranslated(TypeTranslateDTO typeTranslateDTO) {
+	public int modifyTypeNameTranslated(PrimaryTypeDetailDTO PrimaryTypeDetailDTO) {
 		// Profiling 해보니 Type_name_translated IS NULL 확인하는 게 update 할 때 더 손해라 안 넣는 게 나을 듯
 		// Short-circuit evaluation을 하기 때문인 듯
 		String sql = "UPDATE Type SET Type_name_translated = ? WHERE Type_name = ?;";
-		return jdbcTemplate.update(sql, typeTranslateDTO.getPrimaryTypeDisplayName(), typeTranslateDTO.getPrimaryType());
+		return jdbcTemplate.update(sql, PrimaryTypeDetailDTO.getPrimaryTypeDisplayName(), PrimaryTypeDetailDTO.getPrimaryType());
 	}
 
 	@Override
 	public int addPlaceType(PlaceTypeCreateDTO placeTypeCreateDTO) {
-		String sql = "INSERT IGNORE INTO Place_type VALUES(?, (SELECT Type_seq FROM Type WHERE Type_name = ?), ?) ;";
-		return jdbcTemplate.update(sql, placeTypeCreateDTO.getPlaceSeq(), placeTypeCreateDTO.getType(), placeTypeCreateDTO.isPrimaryType());
+		String sql = "INSERT IGNORE INTO Place_type(Place_seq, Type_seq) VALUES(?, (SELECT Type_seq FROM Type WHERE Type_name = ?)) ;";
+		return jdbcTemplate.update(sql, placeTypeCreateDTO.getPlaceSeq(), placeTypeCreateDTO.getType());
+	}
+
+	@Override
+	public int modifyPrimaryType(PrimaryTypeUpdateDTO primaryTypeUpdateDTO) {
+		String sql = "UPDATE Place_type SET Is_primary_type = TRUE WHERE Place_seq = ? AND Type_seq = (SELECT Type_seq FROM Type WHERE Type_name = ?);";
+		return jdbcTemplate.update(sql, primaryTypeUpdateDTO.getPlaceSeq(), primaryTypeUpdateDTO.getPrimaryType());
 	}
 
 }
