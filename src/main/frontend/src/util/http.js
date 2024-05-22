934e35e2e40f4723fd4e06a89c914d4556677a7f
Soroso <tudgus010707@naver.com>
2024-05-22 13:29:00 +0900

    ✨ Gpt prompt 추가

--- src/main/frontend/src/util/http.js
+++ src/main/frontend/src/util/http.js
@@ -50,18 +50,27 @@ export const generateCase = async (city, userSpots, totalDates) => {
     messages: [
       {
         role: "system",
-        content: `너는 여행 동선을 분배해주는 인공지능이야. ${city}를 ${
-          totalDates + 1
-        }일 동안 여행하는 계획을 짜고 있어
-    내가 고른 여행지는 다음과 같아. 모든 쓸데없는 말과 해당 여행지에 대한 정보나 설명을 생략하고 결과값을 json형태로 줘(마크다운 문법은 생략해줘.). 예시는 다음과 같아.
-    {'day1':['나리타공항', '도쿄역','신주쿠 XX식당', '시부야 스크램블 교차로','XX호텔'],'day2':['요코하마 XX식당', '요코하마 차이나타운', '나리타 공항']}
-    ${userSpots.map((spot) => {
-      return `${spot.spotName}(위도:${spot.latitude},경도: ${spot.longitude}),`;
-    })}`,
+        content: `너는 여행 동선을 분배해주는 인공지능이야. 
+              도시와 여행지 목록, 여행 일정을 보고 날짜별로 여행지들을 적절하게 배분해줘.
+              나는 ${city}를 ${totalDates + 1}일 동안 여행하는 계획을 짜고 있어
+              내가 고른 여행지는 다음과 같아. 
+              ${userSpots.map((spot) => {
+                return `${spot.spotName}(id:${spot.id},위도:${spot.latitude},경도: ${spot.longitude}),`;
+              })}
+              모든 쓸데없는 말과 해당 여행지에 대한 정보나 설명을 생략하고 결과값을 json형태로 줘(마크다운 문법은 생략해줘.). 예시는 다음과 같아.
+              {
+                "day1":[{"id": "ChIJVze90XnzImARoRp3YqEpbtU", "spotName": "나리타공항", "latitude":35.770178, "longitude":140.3843215}, {"id": "ChIJVze90XnzImARoRp3YqEpbtU", "spotName": "나리타공항", "latitude":35.770178, "longitude":140.3843215}],
+              "day2": [{"id": "ChIJVze90XnzImARoRp3YqEpbtU", "spotName": "나리타공항", "latitude":35.770178, "longitude":140.3843215}, {"id": "ChIJVze90XnzImARoRp3YqEpbtU", "spotName": "나리타공항", "latitude":35.770178, "longitude":140.3843215}]
+            }
+              공항 입력시에는 첫날 첫 여행지와 마지막날 마지막 여행지는 공항 혹은 항구로 넣어줘. 호텔 등 숙소 입력시에는 마지막날을 제외한 매일의 마지막 일정으로 넣어줘.
+              그리고 내가 선택한 여행지 외에는 절대 넣지마. `,
       },
     ],
     model: "gpt-4o",
   });
 
-  console.log(completion.choices[0]);
+  // console.log(completion.choices[0]);
+  const gptAnswer = completion.choices[0].message.content;
+  console.log(gptAnswer);
+  return gptAnswer;
 };
