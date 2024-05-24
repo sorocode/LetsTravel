02ef97a0018c1ff9759bd5a17e2b8cb137ea161e
KoharuDaisukii <bong1926@naver.com>
2024-05-24 15:41:45 +0900

    AllArgsContructor annotation 추가

--- src/main/java/com/letsTravel/LetsTravel/domain/Location.java
+++ src/main/java/com/letsTravel/LetsTravel/domain/Location.java
@@ -1,5 +1,6 @@
 package com.letsTravel.LetsTravel.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Location {
 
 	private float latitude;
