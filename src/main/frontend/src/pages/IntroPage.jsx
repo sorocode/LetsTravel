e173b2ddfea638481cf272d675c1d15278ad9e0a
Soroso <tudgus010707@naver.com>
2024-05-28 16:30:09 +0900

    💄 인트로페이지 개선

--- src/main/frontend/src/pages/IntroPage.jsx
+++ src/main/frontend/src/pages/IntroPage.jsx
@@ -1,12 +1,24 @@
 import Button from "../components/UI/Buttons/Button";
-
+import IntroImage from "../assets/images/plainview.jpg";
+import { Link } from "react-router-dom";
 const IntroPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center mt-36">
-      <h1 className="text-2xl font-bold">LetsTravel</h1>
-      <Button to="select" color="#B7E2FF">
-        ✈️여행 일정 만들러가기
-      </Button>
+    <div
+      className="flex flex-col justify-center items-center h-screen"
+      style={{
+        background: `url(${IntroImage}) center`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Link to="select">
+        <div className="flex flex-col justify-center items-center">
+          <span className="text-2xl font-semibold font-ShadowsIntoLight">
+            LetsTravel
+          </span>
+          <span>여행도 스마트하게</span>
+        </div>
+      </Link>
     </div>
   );
 };
--- src/main/frontend/src/pages/RootLayout.jsx
+++ src/main/frontend/src/pages/RootLayout.jsx
@@ -1,15 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "../components/UI/NavBar";
 
 const RootLayout = () => {
+  const location = useLocation();
+
   return (
     <>
       <main>
         <Outlet />
       </main>
-      <nav className="fixed bottom-0 z-[1000] flex justify-center items-center min-w-full">
-        <NavBar />
-      </nav>
+      {location.pathname !== "/" && (
+        <nav className="fixed bottom-0 z-[1000] flex justify-center items-center min-w-full">
+          <NavBar />
+        </nav>
+      )}
     </>
   );
 };
