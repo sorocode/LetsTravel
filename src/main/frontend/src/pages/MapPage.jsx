a8ed8c3212c6763c7dedecd05ce8b2714f353590
Soroso <tudgus010707@naver.com>
2024-05-29 10:00:28 +0900

    ✨ 여행계획 타임라인 템플릿 추가

--- src/main/frontend/src/pages/MapPage.jsx
+++ src/main/frontend/src/pages/MapPage.jsx
@@ -2,55 +2,68 @@ import MapContainer from "../components/UI/MapContainer";
 import { AdvancedMarker } from "@vis.gl/react-google-maps";
 import { useSelector } from "react-redux";
 import BottomSheet from "../components/UI/Bottomsheet/BottomSheet";
+import Button from "../components/UI/Buttons/Button";
 
+import Itinery from "../components/UI/Itinery";
 function MapPage() {
   const scheduleData = useSelector((state) => state.schedule.selectedSchedule);
-  console.log(scheduleData);
-  const days = Object.keys(scheduleData);
 
+  const days =
+    scheduleData !== undefined ? Object.keys(scheduleData) : undefined;
   return (
     <>
-      <MapContainer>
-        {days.map((day) =>
-          scheduleData[day].map((spot) => {
-            const spotId = spot.id;
-            const pos = {
-              lat: spot.latitude,
-              lng: spot.longitude,
-            };
-            console.log("pos", pos);
-            let bgColor;
-            console.log("day", day);
-            switch (parseInt(day)) {
-              case 1:
-                bgColor = "#F3B385";
-                break;
-              case 2:
-                bgColor = "#c985f3";
-                break;
-              case 3:
-                bgColor = "#85F389";
-                break;
-              default:
-                bgColor = "#7BC9FF";
-                break;
-            }
-            return (
-              <div key={spotId}>
-                <AdvancedMarker position={pos}>
-                  <div
-                    style={{ backgroundColor: bgColor }}
-                    className="rounded-[86px] border-solid border-[#000000] border p-2.5 flex flex-col gap-2.5 items-center justify-center shrink-0 w-[30px] h-[30px] relative "
-                  >
-                    <span>{day}</span>
+      {days !== undefined ? (
+        <div>
+          <MapContainer>
+            {days.map((day) =>
+              scheduleData[day].map((spot) => {
+                const spotId = spot.id;
+                const pos = {
+                  lat: spot.latitude,
+                  lng: spot.longitude,
+                };
+                console.log("pos", pos);
+                let bgColor;
+                console.log("day", day);
+                switch (parseInt(day)) {
+                  case 1:
+                    bgColor = "#F3B385";
+                    break;
+                  case 2:
+                    bgColor = "#c985f3";
+                    break;
+                  case 3:
+                    bgColor = "#85F389";
+                    break;
+                  default:
+                    bgColor = "#7BC9FF";
+                    break;
+                }
+                return (
+                  <div key={spotId}>
+                    <AdvancedMarker position={pos}>
+                      <div
+                        style={{ backgroundColor: bgColor }}
+                        className="rounded-[86px] border-solid border-[#000000] border p-2.5 flex flex-col gap-2.5 items-center justify-center shrink-0 w-[30px] h-[30px] relative "
+                      >
+                        <span>{day}</span>
+                      </div>
+                    </AdvancedMarker>
                   </div>
-                </AdvancedMarker>
-              </div>
-            );
-          })
-        )}
-      </MapContainer>
-      <BottomSheet></BottomSheet>
+                );
+              })
+            )}
+          </MapContainer>
+          <BottomSheet>
+            <Itinery />
+          </BottomSheet>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center mt-20">
+          <p>지도를 보려면 여행지를 추가해주세요!</p>
+          <Button to="/select">바로가기</Button>
+        </div>
+      )}
     </>
   );
 }
