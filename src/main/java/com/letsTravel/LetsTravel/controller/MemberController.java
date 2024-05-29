fd5679650711451c9e31a4c068b1ebbb2e5e40bf
KoharuDaisukii <bong1926@naver.com>
2024-05-29 14:08:36 +0900

    사용하지 않은 import 문 제거

--- src/main/java/com/letsTravel/LetsTravel/controller/MemberController.java
+++ src/main/java/com/letsTravel/LetsTravel/controller/MemberController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.letsTravel.LetsTravel.domain.LoginDTO;
