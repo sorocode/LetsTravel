7a8b7f50a6053e3c7924508d1dd605890514c2be
Soroso <tudgus010707@naver.com>
2024-05-29 19:40:55 +0900

    💄 커스텀 마커 색깔 추가

--- src/main/frontend/src/components/UI/Itinery.jsx
+++ src/main/frontend/src/components/UI/Itinery.jsx
@@ -17,40 +17,61 @@ export default function Itinery({ days, scheduleData }) {
     <>
       <Timeline position="alternate">
         {days.map((day) => {
-          return scheduleData[parseInt(day)].map((spot) => {
-            const spotId = spot.spotId;
-            const spotName = spot.spotName;
-            console.log(spotName);
-            return (
-              <>
-                <TimelineItem key={spotId}>
-                  <TimelineOppositeContent
-                    sx={{ m: "auto 0" }}
-                    align="right"
-                    variant="body2"
-                    color="text.secondary"
-                  >
-                    9:30 am
-                  </TimelineOppositeContent>
-                  <TimelineSeparator>
-                    <TimelineConnector />
-                    <TimelineDot>
-                      <CustomMarker>
-                        {scheduleData[day].indexOf(spot) + 1}
-                      </CustomMarker>
-                    </TimelineDot>
-                    <TimelineConnector />
-                  </TimelineSeparator>
-                  <TimelineContent sx={{ py: "12px", px: 2 }}>
-                    <Typography variant="h6" component="span">
-                      {spotName}
-                    </Typography>
-                    <Typography>관광명소</Typography>
-                  </TimelineContent>
-                </TimelineItem>
-              </>
-            );
-          });
+          let bgColor;
+          console.log("day", day);
+          switch (parseInt(day)) {
+            case 1:
+              bgColor = "#F3B385";
+              break;
+            case 2:
+              bgColor = "#c985f3";
+              break;
+            case 3:
+              bgColor = "#85F389";
+              break;
+            default:
+              bgColor = "#7BC9FF";
+              break;
+          }
+          return (
+            <>
+              <span className="text-xl">Day{day}</span>
+              {scheduleData[parseInt(day)].map((spot) => {
+                const spotId = spot.spotId;
+                const spotName = spot.spotName;
+                console.log(spotName);
+                return (
+                  <>
+                    <TimelineItem key={spotId}>
+                      <TimelineOppositeContent
+                        sx={{ m: "auto 0" }}
+                        align="right"
+                        variant="body2"
+                        color="text.secondary"
+                      >
+                        9:30 am
+                      </TimelineOppositeContent>
+                      <TimelineSeparator>
+                        <TimelineConnector />
+                        {/* <TimelineDot> */}
+                        <CustomMarker color={bgColor}>
+                          {scheduleData[day].indexOf(spot) + 1}
+                        </CustomMarker>
+                        {/* </TimelineDot> */}
+                        <TimelineConnector />
+                      </TimelineSeparator>
+                      <TimelineContent sx={{ py: "12px", px: 2 }}>
+                        <Typography variant="h6" component="span">
+                          {spotName}
+                        </Typography>
+                        <Typography>관광명소</Typography>
+                      </TimelineContent>
+                    </TimelineItem>
+                  </>
+                );
+              })}
+            </>
+          );
         })}
       </Timeline>
     </>
