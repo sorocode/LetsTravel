4067885c7c17b11e0feee8db84a89216b7c65a36
Soroso <tudgus010707@naver.com>
2024-05-29 19:40:04 +0900

    💄 닫기 버튼 동적 표출

--- src/main/frontend/src/components/UI/Bottomsheet/SheetHeader.jsx
+++ src/main/frontend/src/components/UI/Bottomsheet/SheetHeader.jsx
@@ -1,8 +1,8 @@
-const SheetHeader = ({ children, onClick }) => {
+const SheetHeader = ({ children, onClick, isClose }) => {
   return (
     <div className="flex items-center justify-between h-[52px] border-b-[1px] border-[#e0e0e0] px-[16px] py-0">
       <span className="title">{children}</span>
-      <button onClick={onClick}>닫기</button>
+      {!isClose && <button onClick={onClick}>닫기</button>}
     </div>
   );
 };
