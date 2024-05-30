335495513381ddbfaa32659c0c07a67a0d8551e7
Soroso <tudgus010707@naver.com>
2024-05-30 22:28:36 +0900

    💄 나의 여행 조회 아이콘 추가

--- src/main/frontend/src/components/UI/Header.jsx
+++ src/main/frontend/src/components/UI/Header.jsx
@@ -2,20 +2,24 @@ import { Link } from "react-router-dom";
 import logoIcon from "/logo.png";
 const Header = () => {
   return (
-    <Link to="/">
-      <div className="flex justify-start mx-8 gap-2 mt-5 mb-2">
-        <img
-          src={logoIcon}
-          alt="logoIcon"
-          width="10%"
-          height="8%"
-          className="rounded-md"
-        />
-        <span className="text-xl font-semibold font-ShadowsIntoLight">
-          LetsTravel
-        </span>
-      </div>
-    </Link>
+    <div className="flex items-center mx-8">
+      <Link to="/">
+        <div className="flex justify-start gap-2 mt-5 mb-2">
+          <img
+            src={logoIcon}
+            alt="logoIcon"
+            width="10%"
+            height="8%"
+            className="rounded-md"
+          />
+          <span className="text-xl font-semibold font-ShadowsIntoLight">
+            LetsTravel
+          </span>
+        </div>
+      </Link>
+
+      <img src="/suitcase.svg" alt="suitcase" width="10%" />
+    </div>
   );
 };
 
