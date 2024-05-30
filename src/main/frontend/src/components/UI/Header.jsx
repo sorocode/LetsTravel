1c31b12ad6ea569959deccf7eaa63330bf78a51b
Soroso <tudgus010707@naver.com>
2024-05-30 11:45:16 +0900

    ✨ 라우터 변경

--- src/main/frontend/src/components/UI/Header.jsx
+++ src/main/frontend/src/components/UI/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import logoIcon from "/logo.png";
 const Header = () => {
   return (
-    <Link to="/select">
+    <Link to="/">
       <div className="flex justify-start mx-8 gap-2 mt-5 mb-2">
         <img src={logoIcon} alt="logoIcon" width="10%" height="8%" />
         <span className="text-xl font-semibold font-ShadowsIntoLight">
