eb5852b78f16cab2e64908b7ddae5a7ba21b1ef0
KoharuDaisukii <bong1926@naver.com>
2024-05-31 14:39:49 +0900

    Type 테이블이 더럽혀져서 임시로 방어

--- src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplateTypeRepository.java
+++ src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplateTypeRepository.java
@@ -23,7 +23,7 @@ public class JdbcTemplateTypeRepository implements TypeRepository {
 		// Profiling 해보니 Type_name_translated IS NULL 확인하는 게 update 할 때 더 손해라 안 넣는 게 나을 듯
 		// Short-circuit evaluation을 하기 때문인 듯
 		// 다시 생각해 보니 primaryTypeDisplayName이 안 넘어오는 경우가 있어 번역이 수정되면 안 되니 IS NULL이 필요할 듯
-		String sql = "UPDATE Type SET Type_name_translated = ? WHERE Type_name = ? AND Type_name_translated IS NULL;";
+		String sql = "UPDATE Type SET Type_name_translated = ? WHERE Type_name = ? AND Type_name_translated; --IS NULL;";
 		return jdbcTemplate.update(sql, PrimaryTypeDetailDTO.getPrimaryTypeDisplayName(), PrimaryTypeDetailDTO.getPrimaryType());
 	}
 
