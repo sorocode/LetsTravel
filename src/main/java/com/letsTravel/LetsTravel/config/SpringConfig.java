ecb840378a69eba6fa853bafb73b974c7dcc698e
KoharuDaisukii <bong1926@naver.com>
2024-05-31 11:10:35 +0900

    CORS 설정 추가

--- src/main/java/com/letsTravel/LetsTravel/config/SpringConfig.java
+++ src/main/java/com/letsTravel/LetsTravel/config/SpringConfig.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SpringConfig {
@@ -16,4 +18,16 @@ public class SpringConfig {
 	public SpringConfig(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:5173") // Vite 개발 서버 주소
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
+			}
+		};
+	}
+
 }
