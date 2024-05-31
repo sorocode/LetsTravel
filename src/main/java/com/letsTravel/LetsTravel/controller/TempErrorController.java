a9a371c1def7217de51ef643ab2a4ef7081f051e
KoharuDaisukii <bong1926@naver.com>
2024-05-31 14:23:05 +0900

    Error 발생 시 main 화면으로 redirect 되던 것을 기존 error page로 변경

--- src/main/java/com/letsTravel/LetsTravel/controller/TempErrorController.java
+++ src/main/java/com/letsTravel/LetsTravel/controller/TempErrorController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
-@Controller
 public class TempErrorController implements ErrorController{
 
 	@RequestMapping("/error")
