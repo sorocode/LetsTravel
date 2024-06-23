4c21c144f5e32b81c677f5a52a26c01376384074
sorocode <tudgus010707@naver.com>
2024-06-23 17:39:54 +0900

    configure url & envs

--- src/main/frontend/vite.config.js
+++ src/main/frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const serverUrl = import.meta.env.VITE_GCP_URL;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://35.216.83.42/",
+        target: serverUrl,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
