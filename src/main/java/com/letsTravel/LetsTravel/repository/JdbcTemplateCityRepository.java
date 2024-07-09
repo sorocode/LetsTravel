66f49084690fb26caf320047dc4a4b1544ea8935
쵸마맛 <@DESKTOP-FR59AON>
2024-07-09 18:52:25 +0900

    명확성을 위해 id->citySeq 네이밍 변경

--- src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplateCityRepository.java
+++ src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplateCityRepository.java
@@ -33,7 +33,7 @@ public class JdbcTemplateCityRepository implements CityRepository {
 					@Override
 					public CityReadDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
 						CityReadDTO cityReadDTO = new CityReadDTO();
-						cityReadDTO.setId(rs.getInt(1));
+						cityReadDTO.setCitySeq(rs.getInt(1));
 						cityReadDTO.setCountryCode(countryCode);
 						cityReadDTO.setCityName(rs.getString(2));
 						cityReadDTO.setCityNameTranslated(rs.getString(3));
