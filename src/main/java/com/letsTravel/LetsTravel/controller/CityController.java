969e3d4654f8abfba6e7d0824d2e3a0d15a985aa
쵸마맛 <@DESKTOP-FR59AON>
2024-07-28 16:20:46 +0900

    Implemented 검색 키워드로 도시 검색 API

--- src/main/java/com/letsTravel/LetsTravel/controller/CityController.java
+++ src/main/java/com/letsTravel/LetsTravel/controller/CityController.java
@@ -1,11 +1,10 @@
 package com.letsTravel.LetsTravel.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.letsTravel.LetsTravel.domain.city.CityListWrapper;
@@ -25,6 +24,12 @@ public class CityController {
 	@GetMapping("/city/{country-code}")
 	public CityListWrapper readCityByCountryCode(@PathVariable("country-code") String countryCode) {
 		CityListWrapper cityListWrapper = new CityListWrapper(cityService.findCities(countryCode));
+		return cityListWrapper;
+	}
+
+	@GetMapping("/city")
+	public CityListWrapper readCityByKeyWord(@RequestParam(value = "keyword") String keyword) {
+		CityListWrapper cityListWrapper = new CityListWrapper(cityService.findCitiesByKeyword(keyword));
 		return cityListWrapper;
 	}
 }
