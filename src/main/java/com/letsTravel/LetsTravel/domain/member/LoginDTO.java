307fec2eae96e13990f5098f3488e6ae80f794ec
Choi, Suyeong <tndud25@gmail.com>
2024-07-29 18:52:21 +0900

    dD

--- src/main/java/com/letsTravel/LetsTravel/domain/member/LoginDTO.java
+++ src/main/java/com/letsTravel/LetsTravel/domain/member/LoginDTO.java
@@ -9,6 +9,29 @@ import lombok.Setter;
 @NoArgsConstructor
 public class LoginDTO {
 
+<<<<<<< HEAD
 	private String id;
 	private String pw;
+=======
+	private String loginId;
+	private String password;
+	private String passwordCheck;
+
+	public User toUser() {
+		return User.builder()
+				.loginId(this.loginId)
+				.password(this.password)
+				.role(UserRole.USER)
+				.build();
+	}
+
+	public User toUser(String encodedPassword) {
+		return User.builder()
+				.loginId(this.loginId)
+				.password(encodedPassword)
+				.role(UserRole.USER)
+				.build();
+	}
+
+>>>>>>> ff333ba (일단 커밋)
 }
