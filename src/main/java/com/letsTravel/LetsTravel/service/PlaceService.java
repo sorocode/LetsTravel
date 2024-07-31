f15c5491595746bc77466a6b80120a38f7093deb
쵸마맛 <@DESKTOP-FR59AON>
2024-07-31 15:43:53 +0900

    불필요한 import문 삭제

--- src/main/java/com/letsTravel/LetsTravel/service/PlaceService.java
+++ src/main/java/com/letsTravel/LetsTravel/service/PlaceService.java
@@ -5,9 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StopWatch;
 
 import com.letsTravel.LetsTravel.domain.city.CityCreateDTO;
 import com.letsTravel.LetsTravel.domain.city.PlaceCityCreateDTO;
