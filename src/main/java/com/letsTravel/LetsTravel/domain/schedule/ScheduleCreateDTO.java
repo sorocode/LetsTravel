e3818c462bdc82e51b7096b8de1bd55d83587f13
쵸마맛 <@DESKTOP-FR59AON>
2024-08-01 18:43:46 +0900

    ScheduleInfoDTO를 ScheduleCreateDTO로 변환해주는 Constructor 추가

--- src/main/java/com/letsTravel/LetsTravel/domain/schedule/ScheduleCreateDTO.java
+++ src/main/java/com/letsTravel/LetsTravel/domain/schedule/ScheduleCreateDTO.java
@@ -2,6 +2,8 @@ package com.letsTravel.LetsTravel.domain.schedule;
 
 import java.sql.Time;
 
+import com.letsTravel.LetsTravel.domain.place.Place;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,4 +18,11 @@ public class ScheduleCreateDTO {
 	private int dateSeq;
 	private int visitSeq;
 	private Time visitTime;
+	
+	public ScheduleCreateDTO(ScheduleInfoDTO scheduleDetailDTO) {
+		this.placeId = scheduleDetailDTO.getPlace().getId();
+		this.dateSeq = scheduleDetailDTO.getDateSeq();
+		this.visitSeq = scheduleDetailDTO.getVisitSeq();
+		this.visitTime = scheduleDetailDTO.getVisitTime();
+	}
 }
