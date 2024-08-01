e401e3e8a75947a8ee800284789359efd8709294
쵸마맛 <@DESKTOP-FR59AON>
2024-08-01 18:45:00 +0900

    Plan의 모든 정보를 담는 DTO

---
+++ src/main/java/com/letsTravel/LetsTravel/domain/plan/PlanDetailDTO.java
@@ -0,0 +1,21 @@
+package com.letsTravel.LetsTravel.domain.plan;
+
+import java.util.List;
+
+import com.letsTravel.LetsTravel.domain.city.CityReadDTO;
+import com.letsTravel.LetsTravel.domain.member.MemberBasicInfoReadDTO;
+import com.letsTravel.LetsTravel.domain.schedule.ScheduleInfoDTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PlanDetailDTO {
+	private PlanInfoDTO planInfo;
+	private List<MemberBasicInfoReadDTO> planShareMembers;
+	private List<CityReadDTO> planCities;
+	private List<ScheduleInfoDTO> schedules;
+}
