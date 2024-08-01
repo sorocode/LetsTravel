16c3516362255407e60f2bf7b558db61267b27b8
쵸마맛 <@DESKTOP-FR59AON>
2024-08-01 18:43:17 +0900

    PlanDetailDTO 전용 Wrapper

---
+++ src/main/java/com/letsTravel/LetsTravel/domain/plan/PlanWrapper.java
@@ -0,0 +1,14 @@
+package com.letsTravel.LetsTravel.domain.plan;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlanWrapper {
+	PlanDetailDTO plan;
+}
