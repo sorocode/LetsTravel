66055c599ec2e79beed4f3013faadda779e5814f
쵸마맛 <@DESKTOP-FR59AON>
2024-08-01 18:42:18 +0900

    Plan_seq로 스케줄 찾는 메소드 추가

--- src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplateScheduleRepository.java
+++ src/main/java/com/letsTravel/LetsTravel/repository/JdbcTemplateScheduleRepository.java
@@ -1,11 +1,16 @@
 package com.letsTravel.LetsTravel.repository;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import com.letsTravel.LetsTravel.domain.place.Place;
 import com.letsTravel.LetsTravel.domain.schedule.ScheduleCreateDTO;
+import com.letsTravel.LetsTravel.domain.schedule.ScheduleInfoDTO;
 import com.letsTravel.LetsTravel.domain.schedule.ScheduleUpdateDTO;
 
 @Repository
@@ -20,18 +25,34 @@ public class JdbcTemplateScheduleRepository implements ScheduleRepository {
 	@Override
 	public int addSchedule(ScheduleCreateDTO scheduleCreateDTO) {
 		boolean isVisitTimeNull = scheduleCreateDTO.getVisitTime() == null;
-		String sql = "INSERT INTO Schedule(Plan_seq, Place_seq, Date_seq, Visit_seq"
-				+ (isVisitTimeNull ? "" : ", Visit_time")
-				+ ") VALUE(?, (SELECT Place_seq FROM Place WHERE Place_id = ?), ?, ?" + (isVisitTimeNull ? "" : ", ?")
-				+ ");";
-		return jdbcTemplate.update(sql, scheduleCreateDTO.getPlanSeq(), scheduleCreateDTO.getPlaceId(),
-				scheduleCreateDTO.getDateSeq(), scheduleCreateDTO.getVisitSeq(), scheduleCreateDTO.getVisitTime());
+		String sql = "INSERT INTO Schedule(Plan_seq, Place_seq, Date_seq, Visit_seq" + (isVisitTimeNull ? "" : ", Visit_time") + ") VALUE(?, (SELECT Place_seq FROM Place WHERE Place_id = ?), ?, ?"
+				+ (isVisitTimeNull ? "" : ", ?") + ");";
+		return jdbcTemplate.update(sql, scheduleCreateDTO.getPlanSeq(), scheduleCreateDTO.getPlaceId(), scheduleCreateDTO.getDateSeq(), scheduleCreateDTO.getVisitSeq(),
+				scheduleCreateDTO.getVisitTime());
 	}
 
 	@Override
 	public int modifySchedule(ScheduleUpdateDTO scheduleUpdateDTO) {
 		String sql = "UPDATE Schedule SET Place_seq = ?, Date_seq = ?, Visit_seq = ?, Visit_time = ? WHERE Schedule_seq = ?";
-		return jdbcTemplate.update(sql, scheduleUpdateDTO.getPlaceSeq(), scheduleUpdateDTO.getDateSeq(),
-				scheduleUpdateDTO.getVisitSeq(), scheduleUpdateDTO.getVisitTime(), scheduleUpdateDTO.getScheduleSeq());
+		return jdbcTemplate.update(sql, scheduleUpdateDTO.getPlaceSeq(), scheduleUpdateDTO.getDateSeq(), scheduleUpdateDTO.getVisitSeq(), scheduleUpdateDTO.getVisitTime(),
+				scheduleUpdateDTO.getScheduleSeq());
+	}
+
+	@Override
+	public List<ScheduleInfoDTO> findSchedulesByPlanSeq(int planSeq) {
+		String sql = "SELECT S.Schedule_seq, S.Place_seq, S.Date_seq, S.Visit_seq, S.Visit_time FROM Schedule S WHERE S.Plan_seq = ?;";
+		return jdbcTemplate.query(sql, scheduleInfoRowMapper(), planSeq);
+	}
+
+	private RowMapper<ScheduleInfoDTO> scheduleInfoRowMapper() {
+		return (rs, rowNum) -> {
+			ScheduleInfoDTO scheduleInfo = new ScheduleInfoDTO();
+			scheduleInfo.setScheduleSeq(rs.getInt("S.Schedule_seq"));
+			scheduleInfo.setPlace(new Place(rs.getInt("S.Place_Seq")));
+			scheduleInfo.setDateSeq(rs.getInt("S.Date_seq"));
+			scheduleInfo.setVisitSeq(rs.getInt("S.Visit_seq"));
+			scheduleInfo.setVisitTime(rs.getTime("S.Visit_time"));
+			return scheduleInfo;
+		};
 	}
 }
--- src/main/java/com/letsTravel/LetsTravel/repository/ScheduleRepository.java
+++ src/main/java/com/letsTravel/LetsTravel/repository/ScheduleRepository.java
@@ -1,12 +1,14 @@
 package com.letsTravel.LetsTravel.repository;
 
+import java.util.List;
+
 import com.letsTravel.LetsTravel.domain.schedule.ScheduleCreateDTO;
+import com.letsTravel.LetsTravel.domain.schedule.ScheduleInfoDTO;
 import com.letsTravel.LetsTravel.domain.schedule.ScheduleUpdateDTO;
 
 public interface ScheduleRepository {
 
 	int addSchedule(ScheduleCreateDTO scheduleCreateDTO);
-
 	int modifySchedule(ScheduleUpdateDTO scheduleUpdateDTO);
-
+	List<ScheduleInfoDTO> findSchedulesByPlanSeq(int planSeq);
 }
